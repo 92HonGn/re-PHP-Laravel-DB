911380b0f00f45bd32e2bc12444957854ca84b2c
謝政宏 <b3194359@gmail.com>
2020-10-30 21:52:01 +0800

    ceate get request api endpoint

--- CRUDAPI/CRUD01/Employees.php
+++ CRUDAPI/CRUD01/Employees.php
@@ -1,12 +1,15 @@
 <?php
+
 class Employee{
 
     // 連線
     private $conn;
 
+    
     // 資料表
     private $db_table = "";
 
+    
     // 欄位
     public $id;
     public $name;
@@ -14,12 +17,23 @@ class Employee{
     public $age;
     public $designation;
     public $created;
+    
 
     // 資料庫連線
     public function __construct($db){
         $this->conn = $db;
     }
+    
+    
+    //全部資料
+    public function getEmployees(){
+        $sqlQuery = "SELECT id, name, email, age, designation, created FROM " . $this->db_table . "";
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute();
+        return $stmt;
+    }
 
+    
     // 建立
     public function createEmployee(){
         $sqlQuery = "INSERT INTO
@@ -53,7 +67,97 @@ class Employee{
 
         return false;
     }
+    
+    
+    
+    
+    // 讀取單筆
+    public function getSingleEmployee(){
+            $sqlQuery = "SELECT
+                        id, 
+                        name, 
+                        email, 
+                        age, 
+                        designation, 
+                        created
+                      FROM
+                        ". $this->db_table ."
+                    WHERE 
+                       id = ?
+                    LIMIT 0,1";
+
+            $stmt = $this->conn->prepare($sqlQuery);
+
+            $stmt->bindParam(1, $this->id);
+
+            $stmt->execute();
+
+            $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->name = $dataRow['name'];
+            $this->email = $dataRow['email'];
+            $this->age = $dataRow['age'];
+            $this->designation = $dataRow['designation'];
+            $this->created = $dataRow['created'];
+    }        
+
+    
+        
+    // 更新
+    public function updateEmployee(){
+            $sqlQuery = "UPDATE
+                        ". $this->db_table ."
+                    SET
+                        name = :name, 
+                        email = :email, 
+                        age = :age, 
+                        designation = :designation, 
+                        created = :created
+                    WHERE 
+                        id = :id";
+        
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            $this->name=htmlspecialchars(strip_tags($this->name));
+            $this->email=htmlspecialchars(strip_tags($this->email));
+            $this->age=htmlspecialchars(strip_tags($this->age));
+            $this->designation=htmlspecialchars(strip_tags($this->designation));
+            $this->created=htmlspecialchars(strip_tags($this->created));
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            
+            $stmt->bindParam(":name", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":age", $this->age);
+            $stmt->bindParam(":designation", $this->designation);
+            $stmt->bindParam(":created", $this->created);
+            $stmt->bindParam(":id", $this->id);
+        
+            if($stmt->execute()){
+               return true;
+            }
+            return false;
+    }
+
+    
+    
+    // 刪除
+    function deleteEmployee(){
+            $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = ?";
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            $stmt->bindParam(1, $this->id);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+    }
+
 
 
 }
+
 ?>
